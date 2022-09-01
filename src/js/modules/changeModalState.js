49b0d08b7e93eb22e75b6f63b96f4f89eb232953
Def <deferenseo@gmail.com>
2022-09-01 16:22:32 +0300

    added checkNumInputs function, started changeModalState.js

---
+++ src/js/modules/changeModalState.js
@@ -0,0 +1,24 @@
+import checkNumInputs from "./checkNumInputs";
+
+const changeModalState = (state) => {
+    const windowForm = document.querySelectorAll('.balcon_icons_img');
+    const windowWidth = document.querySelector('#width');
+    const windowHeight = document.querySelector('#height');
+    const windowType = document.querySelector('#view_type');
+    const windowProfile = document.querySelectorAll('.checkbox');
+
+    checkNumInputs('#width');
+    checkNumInputs('#height');
+
+    function bindActionToElems(event, elem, prop) {
+        elem.forEach((item, i) => {
+            item.addEventListener(event, () => {
+                state[prop] = i;
+                console.log(state);
+            });
+        });
+    }
+    bindActionToElems('click', windowForm, 'form');
+};
+
+export default changeModalState;
